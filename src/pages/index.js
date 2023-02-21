3713e8bfcf670e6592ecaa9c0e19facd995ee644
dquoupb <45218533+dquoupb@users.noreply.github.com>
2023-02-21 10:39:52 +0900

    파일추가작업
    파일추가작업

---
+++ src/pages/index.js
@@ -0,0 +1,26 @@
+import Link from 'next/link';
+
+export default function Home() {
+  return (
+    <>
+      <header>
+        <h1>
+          <Link href="/">WEB</Link>
+        </h1>
+        <ol>
+          <li><Link href="/read/1">html</Link></li>
+          <li><Link href="/read/2">css</Link></li>
+        </ol>
+        <article>
+          <h2>Welcome</h2>
+          Hello, WEB!!
+        </article>
+        <ul>
+          <li><Link href="/create">create</Link></li>
+          <li><Link href="/update">update</Link></li>
+          <li><Link href="/delete">delete</Link></li>
+        </ul>
+      </header>
+    </>
+  )
+}
